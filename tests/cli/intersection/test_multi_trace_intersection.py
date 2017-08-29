16dca208ac183806e7c1d273de3c5708c4aeffc1
Michael Jeanson <mjeanson@efficios.com>
2017-08-29 17:20:36 -0400

    WIP: convert test_multi_trace_intersection.py to new bindings

--- tests/cli/intersection/test_multi_trace_intersection.py
+++ tests/cli/intersection/test_multi_trace_intersection.py
@@ -22,10 +22,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import bt_python_helper
+import unittest
 import tempfile
-import babeltrace.writer as btw
-import babeltrace.reader as btr
+import bt2
 import uuid
 import shutil
 
@@ -41,29 +40,33 @@ class Packet(object):
     def __init__(self, timestamps):
         self.timestamps = timestamps
 
-clock_uuid = uuid.uuid4()
 
 # stream_descriptions is a list of lists of Packets.
 # Each stream description list is mapped to a single stream instance.
 def create_trace(path, clock_uuid, stream_descriptions):
-    trace = btw.Writer(path)
-    clock = btw.Clock('test_clock')
+    writer = bt2.CtfWriter(path)
+    clock = bt2.CtfWriterClock('test_clock')
     clock.uuid = clock_uuid
-    trace.add_clock(clock)
+    writer.add_clock(clock)
 
-    integer_field_type = btw.IntegerFieldDeclaration(32)
+    integer_field_type = bt2.IntegerFieldType(32)
 
-    event_class = btw.EventClass('simple_event')
-    event_class.add_field(integer_field_type, 'int_field')
+    event_class = bt2.EventClass('simple_event')
+    event_class.payload_field_type.append_field('int_field', integer_field_type)
 
-    stream_class = btw.StreamClass('test_stream')
+    stream_class = bt2.StreamClass('test_stream')
+    stream_class.packet_context_field_type = bt2.StructureFieldType()
+    stream_class.packet_context_field_type.append_field('packet_size', integer_field_type)
+    stream_class.packet_context_field_type.append_field('content_size', integer_field_type)
     stream_class.add_event_class(event_class)
     stream_class.clock = clock
+
+    writer.trace.add_stream_class(stream_class)
 
     streams = []
     stream_entries = []
     for stream_id, stream_packets in enumerate(stream_descriptions):
-        stream = trace.create_stream(stream_class)
+        stream = stream_class()
         streams.append(stream)
 
         for packet in stream_packets:
@@ -76,15 +79,15 @@ def create_trace(path, clock_uuid, stream_descriptions):
     # events to insert in the streams.
     for entry in sorted(stream_entries, key=lambda entry: entry.timestamp):
         clock.time = entry.timestamp
-        event = btw.Event(event_class)
-        event.payload('int_field').value = entry.stream_id
+        event = event_class()
+        event.payload_field['int_field'] = entry.stream_id
         streams[entry.stream_id].append_event(event)
         if entry.end_of_packet is True:
             streams[entry.stream_id].flush()
 
 
 def check_trace_expected_timestamps(trace_paths, expected_timestamps):
-    traces = btr.TraceCollection(intersect_mode=True)
+    traces = bt2.TraceCollection(intersect_mode=True)
     for trace_path in trace_paths:
         trace_handle = traces.add_trace(trace_path, 'ctf')
         if trace_handle is None:
@@ -104,57 +107,60 @@ def print_test_result(test_number, result, description):
     result_string += ' {} - {}'.format(test_number, description)
     print(result_string)
 
-TEST_COUNT = 3
-# TAP plan
-print('1..{}'.format(TEST_COUNT))
+class MultiTraceIntersectionTestCase(unittest.TestCase):
+    def setUp(self):
+        self.clock_uuid = uuid.uuid4()
+        self.trace_path_early = tempfile.mkdtemp()
 
-trace_path_early = tempfile.mkdtemp()
+        # The stream intersection of this trace is event timestamps 11, 12 and 13,
+        # accounting for 9 events in stream-intersection mode
+        print('# Creating early trace at {}'.format(self.trace_path_early))
+        create_trace(self.trace_path_early, self.clock_uuid, [
+            [Packet(range(1, 7)), Packet(range(11, 18))],
+            [Packet(range(8, 15)), Packet(range(22, 24)), Packet(range(30, 60))],
+            [Packet(range(11, 14))]
+            ])
 
-# The stream intersection of this trace is event timestamps 11, 12 and 13,
-# accounting for 9 events in stream-intersection mode
-print('# Creating early trace at {}'.format(trace_path_early))
-create_trace(trace_path_early, clock_uuid, [
-    [Packet(range(1, 7)), Packet(range(11, 18))],
-    [Packet(range(8, 15)), Packet(range(22, 24)), Packet(range(30, 60))],
-    [Packet(range(11, 14))]
-    ])
+        self.trace_path_late = tempfile.mkdtemp()
+        print('# Creating late trace at {}'.format(self.trace_path_late))
+        # The stream intersection of this trace is event timestamps 100, 101, 102, 103
+        # and 104 accounting for 15 events in stream-intersection mode
+        create_trace(self.trace_path_late, self.clock_uuid, [
+            [Packet(range(100, 105)), Packet(range(109, 120))],
+            [Packet(range(88, 95)), Packet(range(96, 110)), Packet(range(112, 140))],
+            [Packet(range(99, 105))]
+            ])
 
-trace_path_late = tempfile.mkdtemp()
-print('# Creating late trace at {}'.format(trace_path_late))
-# The stream intersection of this trace is event timestamps 100, 101, 102, 103
-# and 104 accounting for 15 events in stream-intersection mode
-create_trace(trace_path_late, clock_uuid, [
-    [Packet(range(100, 105)), Packet(range(109, 120))],
-    [Packet(range(88, 95)), Packet(range(96, 110)), Packet(range(112, 140))],
-    [Packet(range(99, 105))]
-    ])
+        self.expected_timestamps_early = []
+        for ts in range(11, 14):
+            for stream in range(3):
+                self.expected_timestamps_early.append(ts)
 
-expected_timestamps_early = []
-for ts in range(11, 14):
-    for stream in range(3):
-        expected_timestamps_early.append(ts)
+        self.expected_timestamps_late = []
+        for ts in range(100, 105):
+            for stream in range(3):
+                self.expected_timestamps_late.append(ts)
 
-expected_timestamps_late = []
-for ts in range(100, 105):
-    for stream in range(3):
-        expected_timestamps_late.append(ts)
-
-expected_timestamps_union = (expected_timestamps_early +
-                            expected_timestamps_late)
+        self.expected_timestamps_union = (self.expected_timestamps_early +
+                                    self.expected_timestamps_late)
 
 # Validate that the stream-intersection mode on a single trace returns only
 # the events that are in overlapping packets across all of the trace's streams.
-result = check_trace_expected_timestamps([trace_path_early],
-                                            expected_timestamps_early)
-print_test_result(1, result, 'Validating expected event timestamps of "early" trace')
+    def test_early_timestamps(self):
+        '''Validating expected event timestamps of "early" trace'''
+        self.assertTrue(check_trace_expected_timestamps([self.trace_path_early],
+                                            self.expected_timestamps_early))
 
-result = check_trace_expected_timestamps([trace_path_late],
-                                            expected_timestamps_late)
-print_test_result(2, result, 'Validating expected event timestamps of "late" trace')
+    def test_late_timestamps(self):
+        '''Validating expected event timestamps of "late" trace'''
+        self.assertTrue(check_trace_expected_timestamps([self.trace_path_late],
+                                            self.expected_timestamps_late))
 
-result = check_trace_expected_timestamps([trace_path_early, trace_path_late],
-                                            expected_timestamps_union)
-print_test_result(3, result, 'Validating expected event timestamps of the union of "early" and "late" traces')
+    def test_union_timestamps(self):
+        '''Validating expected event timestamps of the union of "early" and "late" traces'''
+        self.assertTrue(check_trace_expected_timestamps([self.trace_path_early, self.trace_path_late],
+                                            self.expected_timestamps_union))
 
-shutil.rmtree(trace_path_early)
-shutil.rmtree(trace_path_late)
+    def tearDown(self):
+        shutil.rmtree(self.trace_path_early)
+        shutil.rmtree(self.trace_path_late)
