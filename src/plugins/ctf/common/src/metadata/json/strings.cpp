898ef70ce733ca50be65ec5bdda2f3eed853b535
Philippe Proulx <eeppeliteloop@gmail.com>
2023-12-11 16:51:28 -0500

    Implement CTF 2 JSON requirements
    This patch implements all the CTF 2 JSON requirements.
    
    The internal "public" API is the `Ctf2JsonAnyFragmentValReq` PImpl class
    which validates a single CTF 2 fragment. That's all the CTF 2 metadata
    stream parser will need.
    
    Here's an example:
    
    Code:
        Ctf2JsonAnyFragValReq {}.validate(*bt2c::parseJson(jsonText));
    
    JSON text:
        {
          "type": "data-stream-class",
          "namespace": "lol",
          "name": "salut",
          "packet-context-field-class": {
            "type": "structure",
            "member-classes": [
              {
                "name": "meow",
                "field-class": {
                  "type": "null-terminated-string"
                }
              },
              {
                "name": "lel",
                "field-class": "uint16"
              },
              {
                "name": "meow",
                "field-class": {
                  "type": "dynamic-length-array",
                  "length-field-location": {"path": ["lel"]},
                  "element-field-class": {
                    "type": "fixed-length-signed-integer",
                    "length": 23,
                    "byte-order": "little-endian",
                    "alignment": 24,
                    "attributes": {}
                  }
                }
              }
            ]
          }
        }
    
    Exception message:
        [1:1] Invalid data stream class fragment:
        [5:33] In object property `packet-context-field-class`:
        [5:33] Invalid scope field class:
        [5:33] Invalid structure field class:
        [7:23] In object property `member-classes`:
        [18:7] In array element #3:
        [18:7] Invalid structure field member class:
        [20:24] In object property `field-class`:
        [20:24] Invalid dynamic-length array field class:
        [23:34] In object property `element-field-class`:
        [23:34] Invalid fixed-length signed integer field class:
        [27:26] In object property `alignment`:
        [27:26] Invalid alignment: 24 is not a power of two.
    
    I'd say that Ctf2JsonAnyFragValReq::validate() can validate 95 % of a
    CTF 2 metadata stream. See its class comment to learn what it can't do.
    
    There are a few clang-format off/on comments because I hate what it does
    with nested brace-enclosed lists.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ic622a099a1e0aa5daa896cdfa910d24b817a9a5f
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/12717

---
+++ src/plugins/ctf/common/src/metadata/json/strings.cpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2022-2024 Philippe Proulx <pproulx@efficios.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "strings.hpp"
+
+namespace ctf {
+namespace jsonstr {
+
+const char * const accuracy = "accuracy";
+const char * const align = "alignment";
+const char * const attrs = "attributes";
+const char * const bigEndian = "big-endian";
+const char * const bitOrder = "bit-order";
+const char * const byteOrder = "byte-order";
+const char * const clkCls = "clock-class";
+const char * const cycles = "cycles";
+const char * const dataStreamCls = "data-stream-class";
+const char * const dataStreamClsId = "data-stream-class-id";
+const char * const dataStreamId = "data-stream-id";
+const char * const defClkClsId = "default-clock-class-id";
+const char * const defClkTs = "default-clock-timestamp";
+const char * const descr = "description";
+const char * const discEventRecordCounterSnap = "discarded-event-record-counter-snapshot";
+const char * const dynLenArray = "dynamic-length-array";
+const char * const dynLenBlob = "dynamic-length-blob";
+const char * const dynLenStr = "dynamic-length-string";
+const char * const elemFc = "element-field-class";
+const char * const encoding = "encoding";
+const char * const env = "environment";
+const char * const eventRecordCls = "event-record-class";
+const char * const eventRecordClsId = "event-record-class-id";
+const char * const eventRecordCommonCtx = "event-record-common-context";
+const char * const eventRecordCommonCtxFc = "event-record-common-context-field-class";
+const char * const eventRecordHeader = "event-record-header";
+const char * const eventRecordHeaderFc = "event-record-header-field-class";
+const char * const eventRecordPayload = "event-record-payload";
+const char * const eventRecordSpecCtx = "event-record-specific-context";
+const char * const extensions = "extensions";
+const char * const fc = "field-class";
+const char * const fcAlias = "field-class-alias";
+const char * const fixedLenBitArray = "fixed-length-bit-array";
+const char * const fixedLenBitMap = "fixed-length-bit-map";
+const char * const fixedLenBool = "fixed-length-boolean";
+const char * const fixedLenFloat = "fixed-length-floating-point-number";
+const char * const fixedLenSInt = "fixed-length-signed-integer";
+const char * const fixedLenUInt = "fixed-length-unsigned-integer";
+const char * const flags = "flags";
+const char * const freq = "frequency";
+const char * const ftl = "first-to-last";
+const char * const id = "id";
+const char * const len = "length";
+const char * const lenFieldLoc = "length-field-location";
+const char * const littleEndian = "little-endian";
+const char * const ltf = "last-to-first";
+const char * const mappings = "mappings";
+const char * const mediaType = "media-type";
+const char * const memberClasses = "member-classes";
+const char * const metadataStreamUuid = "metadata-stream-uuid";
+const char * const minAlign = "minimum-alignment";
+const char * const name = "name";
+const char * const ns = "namespace";
+const char * const nullTerminatedStr = "null-terminated-string";
+const char * const offsetFromOrigin = "offset-from-origin";
+const char * const optional = "optional";
+const char * const opts = "options";
+const char * const origin = "origin";
+const char * const path = "path";
+const char * const payloadFc = "payload-field-class";
+const char * const pktContentLen = "packet-content-length";
+const char * const pktCtx = "packet-context";
+const char * const pktCtxFc = "packet-context-field-class";
+const char * const pktEndDefClkTs = "packet-end-default-clock-timestamp";
+const char * const pktHeader = "packet-header";
+const char * const pktHeaderFc = "packet-header-field-class";
+const char * const pktMagicNumber = "packet-magic-number";
+const char * const pktSeqNum = "packet-sequence-number";
+const char * const pktTotalLen = "packet-total-length";
+const char * const preamble = "preamble";
+const char * const precision = "precision";
+const char * const prefDispBase = "preferred-display-base";
+const char * const roles = "roles";
+const char * const seconds = "seconds";
+const char * const selFieldLoc = "selector-field-location";
+const char * const selFieldRanges = "selector-field-ranges";
+const char * const specCtxFc = "specific-context-field-class";
+const char * const staticLenArray = "static-length-array";
+const char * const staticLenBlob = "static-length-blob";
+const char * const staticLenStr = "static-length-string";
+const char * const structure = "structure";
+const char * const traceCls = "trace-class";
+const char * const type = "type";
+const char * const uid = "uid";
+const char * const unixEpoch = "unix-epoch";
+const char * const utf16Be = "utf-16be";
+const char * const utf16Le = "utf-16le";
+const char * const utf32Be = "utf-32be";
+const char * const utf32Le = "utf-32le";
+const char * const utf8 = "utf-8";
+const char * const uuid = "uuid";
+const char * const variant = "variant";
+const char * const varLenSInt = "variable-length-signed-integer";
+const char * const varLenUInt = "variable-length-unsigned-integer";
+const char * const version = "version";
+
+} /* namespace jsonstr */
+} /* namespace ctf */
