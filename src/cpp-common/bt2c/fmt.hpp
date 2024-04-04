91730cec9609a4d62c5f387ca3627d2233685982
Simon Marchi <simon.marchi@efficios.com>
2024-04-03 23:01:05 -0400

    cpp-common/bt2c: add format_as() template functions for wise enums
    Add some format_as() functions in the bt2 and bt2c namespaces that will
    apply to all wise enums defined in that namespace.
    
    Change-Id: I4952f062fc36e94925d2f0b093ac09d0fae6fdc0
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/12248
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Tested-by: jenkins <jenkins@lttng.org>

--- src/cpp-common/bt2c/fmt.hpp
+++ src/cpp-common/bt2c/fmt.hpp
@@ -7,10 +7,25 @@
 #include "common/common.h"
 #include "cpp-common/bt2/message.hpp"
 #include "cpp-common/vendor/fmt/format.h" /* IWYU pragma: keep */
+#include "cpp-common/vendor/wise-enum/wise_enum.h"
 
 #include "uuid.hpp"
 
+namespace internal {
+
+template <typename T>
+using EnableIfIsWiseEnum =
+    typename std::enable_if<wise_enum::is_wise_enum<T>::value, const char *>::type;
+
+} /* namespace internal */
+
 namespace bt2 {
+
+template <typename T>
+::internal::EnableIfIsWiseEnum<T> format_as(const T val) noexcept
+{
+    return wise_enum::to_string<T>(val);
+}
 
 inline const char *format_as(const MessageType type)
 {
@@ -20,6 +35,12 @@ inline const char *format_as(const MessageType type)
 } /* namespace bt2 */
 
 namespace bt2c {
+
+template <typename T>
+::internal::EnableIfIsWiseEnum<T> format_as(const T val) noexcept
+{
+    return wise_enum::to_string<T>(val);
+}
 
 inline std::string format_as(const UuidView uuid)
 {
