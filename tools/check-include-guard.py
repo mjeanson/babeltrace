270aa17afe7d73a4ea41871bdfc2c3dfe76ec21c
Simon Marchi <simon.marchi@efficios.com>
2024-05-14 14:08:23 -0400

    tools: add include guard check script
    Add `tools/check-include-guard.py`, whose task is to verify that the
    include guards in a given file is formatted as we want.  Example:
    
        $ ./tools/check-include-guard.py ./src/plugins/ctf/fs-sink/fs-sink-trace.hpp
        In `#ifndef BABELTRACE_PLUGIN_CTF_FS_SINK_FS_SINK_TRACE_H` include guard line: expecting `#ifndef BABELTRACE_PLUGINS_CTF_FS_SINK_FS_SINK_TRACE_HPP`
    
    `tools/check-include-guard.py` supports the `--fix` option, which makes
    it try to fix the include guards in-place (mostly useful for that
    initial pass where we have a lot of stuff to fix).
    
    Add `tools/check-include-guards.sh`, whose task is to find all the
    files we want to check include guards for, and call
    `tools/check-include-guard.py` on them.
    
    Limitations:
    
     - Only tested on GNU/Linux
    
    Change-Id: I7a376fdcf50f92bda20ab50e529d73bcd2faeb03
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/12632

---
+++ tools/check-include-guard.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+# Usage: check-include-guard.py [--fix] FILE
+#
+# Checks (and optionally tries to fix) the C/C++ header include guard
+# names and format of `FILE`.
+#
+# With `--fix`, this script fixes the include guard names in place.
+
+import re
+import sys
+import pathlib
+import argparse
+
+
+class _Oops(Exception):
+    def __init__(self, msg: str):
+        self._msg = msg
+
+    @property
+    def msg(self):
+        return self._msg
+
+
+def _make_expected_ig_name(filename: pathlib.Path):
+    # Normalize `filename` (e.g. remove `..`) and make it relative to
+    # the root of the source tree.
+    root = pathlib.Path(__file__).parent.parent.resolve(strict=True)
+    filename = filename.absolute().resolve(strict=True).relative_to(root)
+
+    expected_ig_name = re.sub(r"[/.-]", "_", str(filename)).upper()
+    expected_ig_name = re.sub(r"^SRC_", "", expected_ig_name)
+    return "BABELTRACE_" + expected_ig_name
+
+
+def _check_file(filename: pathlib.Path, fix: bool):
+    with open(filename) as f:
+        contents = f.read()
+
+    write_file = False
+
+    # Top part
+    top_re = re.compile(r"^(/\*.+?\*/)\n\n#ifndef (\w+)\n#define (\w+)\n", re.DOTALL)
+    top_m = top_re.match(contents)
+
+    if not top_m:
+        raise _Oops(
+            "Top of the file doesn't have the expected form: block comment, empty line, and then two include guard lines"
+        )
+
+    expected_ig_name = _make_expected_ig_name(filename)
+
+    if fix:
+        contents = top_re.sub(
+            rf"\1\n\n#ifndef {expected_ig_name}\n#define {expected_ig_name}\n",
+            contents,
+        )
+        write_file = True
+    else:
+        if top_m.group(2) != expected_ig_name:
+            raise _Oops(
+                f"In `#ifndef {top_m.group(2)}` include guard line: expecting `#ifndef {expected_ig_name}`"
+            )
+
+        if top_m.group(3) != expected_ig_name:
+            raise _Oops(
+                f"In `#define {top_m.group(3)}` include guard line: expecting `#define {expected_ig_name}`"
+            )
+
+    # Bottom part
+    bottom_re = re.compile(r"\n\n#endif(?: /\* (\w+) \*/)?\n$")
+    bottom_m = bottom_re.search(contents)
+
+    if not bottom_m:
+        raise _Oops("Missing final `#endif` include guard line and trailing empty line")
+
+    if fix:
+        contents = bottom_re.sub(f"\n\n#endif /* {expected_ig_name} */\n", contents)
+        write_file = True
+    else:
+        if bottom_m.group(1) != expected_ig_name:
+            raise _Oops(
+                f"In bottom `#endif` include guard line: expecting `#endif /* {expected_ig_name} */`"
+            )
+
+    if write_file:
+        with open(filename, "w") as f:
+            f.write(contents)
+
+
+def _main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "-f",
+        "--fix",
+        action="store_true",
+        help="attempt to fix the include guards of FILE",
+    )
+    argparser.add_argument("FILE")
+    args = argparser.parse_args()
+    filename = pathlib.Path(args.FILE)
+
+    try:
+        _check_file(filename, args.fix)
+    except _Oops as exc:
+        print(f"{filename}: {exc}", file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    _main()
---
+++ tools/check-include-guards.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2024 EfficiOS Inc.
+
+# Change directory to the root of the project, to make `find` simpler
+cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit 1
+
+find src tests \
+	\( -name '*.hpp' -or -name '*.h' \) \
+	! -path '/.git/*' \
+	! -path 'src/argpar/*' \
+	! -path 'src/cpp-common/vendor/*' \
+	! -path 'tests/data/plugins/flt.lttng-utils.debug-info/tp.h' \
+	! -path 'tests/utils/tap/tap.h' \
+	-print0 | xargs -P "$(nproc)" -n1 -t -0 tools/check-include-guard.py "$@"
