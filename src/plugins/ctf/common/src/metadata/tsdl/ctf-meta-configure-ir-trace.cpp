9328f86be150d02d6c4a5f6de046cf7e4dfb8ca4
Simon Marchi <simon.marchi@efficios.com>
2024-05-30 14:26:09 -0400

    src.ctf.fs: use the new metadata stream parser and message iterator
    Update the src.ctf.fs component class to use:
    
      - the new metadata stream parser / IR generator (ctf::src::CtfIrGenerator)
      - the new message iterator (ctf::src::MsgIter)
    
    The new IR generator produces a trace class instance using the new CTF
    IR type, ctf::src::TraceCls.  This instance is then fed to the message
    iterator.  These two changes must therefore be done in lock step.
    
    Add ctf::src::fs::Medium, a Medium for use with ctf::src::ItemSeqIter
    that reads trace data from the filesystem.  It receives an index, which
    can be thought of as the "playlist" of packets that form the logical
    data stream.  Its buf method needs to be able to find a packet given a
    requested offset in that logical data stream.  Therefore, add a new
    `offsetInStream` field in the ctf_fs_ds_index_entry structure, and the
    `updateOffsetsInStream` method to compute them.  Rename
    ctf_fs_ds_index_entry::offset to offsetInFile, for clarity.
    
    Once the buf method selected the right index entry (packet), it maps
    that portion of the file, and see if the returned buffer can include
    some of the following packets in the file, while at it.
    
    Other details about the changes in this patch, in no particular order:
    
     - Make a copy of ctf_trace_class_configure_ir_trace that works on a
       ctf::src::TraceCls instead of a struct ctf_trace_class.  The old one
       will be removed once src.ctf.lttng-live no longer uses it.
    
     - Remove ctf_fs_metadata, the required fields (the decoder / IR
       generator) are now directly in ctf_fs_trace.
    
     - The quirks to apply are saved in the ctf_fs_component object
       directly.  They were previously saved in the old IR object (struct
       ctf_trace_class), but they are not in the new IR object.  They now
       need to be passed to all instantiated message iterators.
    
     - Remove ctf_fs_msg_iter_data::ds_file_group, add
       ctf_fs_msg_iter_data::port_data.  This allows message iterators to
       access the ctf_fs_component and therefore the quirks.  They can still
       access the ds_file_group through the port_data.
    
     - Replace ctf_fs_msg_iter_data::msg_iter (the old message iterator)
       with ctf_fs_msg_iter_data::msgIter (the new message iterator).
    
     - Remove ctf_fs_msg_iter_data::msg_iter_medops_data, this is the data
       for the medium (medops) for the old message iterator.
    
     - Adjust ctf_fs_iterator_init, ctf_fs_iterator_next,
       ctf_fs_iterator_finalize to the new message iterator.
    
     - Adjust ctf_fs_iterator_seek_beginning to the new message iterator.
       The simplest way I found to reset the state to the beginning state is
       to delete the message iterator and instantiate a new one.
    
     - Update add_ds_file_to_ds_file_group to use readPktProps to read the
       stream class and id.
    
     - Change decode_clock_snapshot_after_event, used for working around
       known tracer bugs, to use an ItemSeqIter.  Define two item visitors,
       ClockSnapshotAfterFirstEventItemVisitor and
       ClockSnapshotAfterLastEventItemVisitor, for the two cases.
    
    Change-Id: I8e3ce344c940da2106bdf8320c28724ca360b48a
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8349
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/12744

--- src/plugins/ctf/common/src/metadata/tsdl/ctf-meta-configure-ir-trace.cpp
+++ src/plugins/ctf/common/src/metadata/tsdl/ctf-meta-configure-ir-trace.cpp
@@ -4,8 +4,11 @@
  * Copyright 2019 Philippe Proulx <pproulx@efficios.com>
  */
 
+#include <cstdint>
+
 #include <babeltrace2/babeltrace.h>
 
+#include "cpp-common/bt2c/logging.hpp"
 #include "cpp-common/bt2c/uuid.hpp"
 
 #include "ctf-meta-configure-ir-trace.hpp"
@@ -35,5 +38,59 @@ void ctf_trace_class_configure_ir_trace(struct ctf_trace_class *tc, const bt2::T
         default:
             bt_common_abort();
         }
+    }
+}
+
+void ctf_trace_class_configure_ir_trace(const ctf::src::TraceCls& tc, bt2::Trace irTrace,
+                                        const std::uint64_t mipVersion,
+                                        const bt2c::Logger& parentLogger)
+{
+    bt2c::Logger logger {parentLogger, "PLUGIN/CTF/META/CONFIG-IR-TRACE"};
+
+    if (tc.uid()) {
+        if (mipVersion == 0) {
+            /*
+             * CTF 2 isn't supported under MIP 0, therefore we expect
+             * `tc.uid()` to be a UUID string.
+             */
+            irTrace.uuid(bt2c::Uuid {*tc.uid()});
+        } else {
+            /* MIP ≥ 1: always a UID */
+            irTrace.uid(*tc.uid());
+        }
+    }
+
+    if (tc.env()) {
+        tc.env()->forEach([&irTrace, &logger](const char *name, bt2::ConstValue val) {
+            switch (val.type()) {
+            case bt2::ValueType::SignedInteger:
+                irTrace.environmentEntry(name, val.asSignedInteger().value());
+                break;
+
+            case bt2::ValueType::UnsignedInteger:
+            {
+                auto uval = val.asUnsignedInteger().value();
+
+                if (uval > std::numeric_limits<std::int64_t>::max()) {
+                    BT_CPPLOGW_SPEC(
+                        logger,
+                        "Cannot convert unsigned integer environment entry value to signed integer without overflowing. Skipping environment entry: "
+                        "entry-name=\"{}\", entry-value={}",
+                        name, uval);
+                    break;
+                }
+
+                irTrace.environmentEntry(name, static_cast<std::int64_t>(uval));
+                break;
+            }
+
+            case bt2::ValueType::String:
+                irTrace.environmentEntry(name, val.asString().value().data());
+                break;
+
+            default:
+                bt_common_abort();
+            }
+        });
     }
 }
--- src/plugins/ctf/common/src/metadata/tsdl/ctf-meta-configure-ir-trace.hpp
+++ src/plugins/ctf/common/src/metadata/tsdl/ctf-meta-configure-ir-trace.hpp
@@ -7,8 +7,19 @@
 #ifndef BABELTRACE_PLUGINS_CTF_COMMON_SRC_METADATA_TSDL_CTF_META_CONFIGURE_IR_TRACE_HPP
 #define BABELTRACE_PLUGINS_CTF_COMMON_SRC_METADATA_TSDL_CTF_META_CONFIGURE_IR_TRACE_HPP
 
-#include "cpp-common/bt2/trace-ir.hpp"
+#include <cstdint>
+
+#include "../ctf-ir.hpp"
+
+namespace bt2c {
+
+class Logger;
+
+} /* namespace bt2c */
 
 void ctf_trace_class_configure_ir_trace(struct ctf_trace_class *tc, bt2::Trace ir_trace);
+
+void ctf_trace_class_configure_ir_trace(const ctf::src::TraceCls& tc, bt2::Trace irTrace,
+                                        std::uint64_t mipVersion, const bt2c::Logger& parentLogger);
 
 #endif /* BABELTRACE_PLUGINS_CTF_COMMON_SRC_METADATA_TSDL_CTF_META_CONFIGURE_IR_TRACE_HPP */
