57731356087ed2bb01c24855f5998c88faf79f33
Simon Marchi <simon.marchi@efficios.com>
2024-05-31 13:02:28 -0400

    cpp-common/bt2c: add {fmt} formatter for `bt2::ConstValue`
    I found this useful for debugging, but I suppose it could be used for
    logging as well.
    
    Change-Id: I83b8af3ed706c64f2f66347cf085d0291c9c09fe
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/12831
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- src/cpp-common/bt2c/fmt.hpp
+++ src/cpp-common/bt2c/fmt.hpp
@@ -7,6 +7,7 @@
 #ifndef BABELTRACE_CPP_COMMON_BT2C_FMT_HPP
 #define BABELTRACE_CPP_COMMON_BT2C_FMT_HPP
 
+#include "cpp-common/bt2/value.hpp"
 #include "cpp-common/vendor/fmt/format.h" /* IWYU pragma: keep */
 #include "cpp-common/vendor/wise-enum/wise_enum.h"
 
@@ -26,6 +27,59 @@ template <typename T>
 ::internal::EnableIfIsWiseEnum<T> format_as(const T val) noexcept
 {
     return wise_enum::to_string<T>(val);
+}
+
+inline std::string format_as(const bt2::ConstValue val) noexcept
+{
+    switch (val.type()) {
+    case ValueType::Null:
+        return "null";
+
+    case ValueType::Bool:
+        return val.asBool().value() ? "true" : "false";
+
+    case ValueType::UnsignedInteger:
+        return fmt::format("{}u", val.asUnsignedInteger().value());
+
+    case ValueType::SignedInteger:
+        return fmt::format("{}", val.asSignedInteger().value());
+
+    case ValueType::Real:
+        return fmt::format("{}", val.asReal().value());
+
+    case ValueType::String:
+        return fmt::format("\"{}\"", val.asString().value());
+
+    case ValueType::Array:
+    {
+        std::string ret {'['};
+        const char *maybeComma = "";
+
+        for (const auto elem : val.asArray()) {
+            ret += fmt::format("{}{}", maybeComma, elem);
+            maybeComma = ", ";
+        }
+
+        ret += ']';
+        return ret;
+    }
+
+    case ValueType::Map:
+    {
+        std::string ret {'{'};
+        const char *maybeComma = "";
+
+        val.asMap().forEach([&](const bt2c::CStringView k, const bt2::ConstValue v) {
+            ret += fmt::format("{}{}: {}", maybeComma, k, v);
+            maybeComma = ", ";
+        });
+
+        ret += '}';
+        return ret;
+    }
+    }
+
+    bt_common_abort();
 }
 
 } /* namespace bt2 */
